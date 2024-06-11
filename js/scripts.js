91257269d0e668da3e7851e25b6f401a67b43223
libbyortiz <165864166+libbyortiz@users.noreply.github.com>
2024-06-11 15:50:55 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var burgerBtn = document.querySelector("#burger-container button");
+var mainNavContainer = document.querySelector ("#main-nav");
+
+var mainNavVisible = false;
+
+burgerBtn.addEventListener("click", () =>{
+    console.log("burger clicked");
+
+    // check to see if we can see the main nav
+    if(mainNavVisible === false){
+        mainNavContainer.classList.remove("hideMainNavAtMobile");
+        mainNavVisible = true;
+
+    }else{
+        mainNavContainer.classList.add("hideMainNavAtMobile");
+        mainNavVisible = false;
+    }
+})
+
+mainNavContainer.addEventListener("click", function(){
+    mainNavContainer.classList.add("hideMainNavAtMobile");
+    mainNavVisible = false;
+})
